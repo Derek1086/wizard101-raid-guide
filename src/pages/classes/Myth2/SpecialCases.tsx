d6b5064e89b9d375ed4618a0b4e63adaef9925b1
Derek Avila <derekavila1086@gmail.com>
2024-06-03 18:55:37 -0500

    stupefy fix

--- src/pages/classes/Myth2/SpecialCases.tsx
+++ src/pages/classes/Myth2/SpecialCases.tsx
@@ -2,7 +2,7 @@ import Header from "../../../components/Header";
 import Title from "../../../components/Text/Title";
 import Body from "../../../components/Text/Body";
 import Myth2Fishing from "./Fishing";
-import MythStrat from "../../../components/strats/Stupefy/Myth";
+import WestSkull1Myth from "../../../components/strats/West/WestSkull1Myth";
 import SouthPuzzle from "../../../components/Puzzles/SouthPuzzle";
 import Navigation from "../../../components/Navigation";
 
@@ -12,7 +12,21 @@ const Myth2SpecialCases = () => {
       <Header />
       <Myth2Fishing />
       <Body text="If Storm team is doing Stupefy do the West Skull 1 fight whenever Myth Jade is ready (Leave skull on the ground for Storm Team to pick up)" />
-      <MythStrat />
+      <br />
+      <Body text="Myth Jade pull mob and follow strat (same as Stupefy)" />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "10px",
+          width: "100%",
+        }}
+      >
+        <div style={{ width: "70%" }}>
+          <WestSkull1Myth />
+        </div>
+      </div>
       <Navigation prev="/myth2/northtokenfight" next="/myth2/stupefy" />
       <Title text="Raining West?" />
       <SouthPuzzle />
--- src/pages/classes/MythJade/SpecialCases.tsx
+++ src/pages/classes/MythJade/SpecialCases.tsx
@@ -2,7 +2,7 @@ import Header from "../../../components/Header";
 import Title from "../../../components/Text/Title";
 import Body from "../../../components/Text/Body";
 import MythJadeFishing from "./Fishing";
-import MythStrat from "../../../components/strats/Stupefy/Myth";
+import WestSkull1Myth from "../../../components/strats/West/WestSkull1Myth";
 import SouthPuzzle from "../../../components/Puzzles/SouthPuzzle";
 import CustomImage from "../../../components/CustomImage";
 import Navigation from "../../../components/Navigation";
@@ -20,7 +20,21 @@ const MythJadeSpecialCases = () => {
       />
       <Body text="If you can't do East and West Stupefy, notify Support/Puller and Storm 4 to do Stupefy instead" />
       <Body text="If Storm team is doing Stupefy do the West Skull 1 fight whenever Myth 2 is ready (Leave skull on the ground for Storm Team to pick up)" />
-      <MythStrat />
+      <br />
+      <Body text="Pull mob and follow strat (same as Stupefy)" />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "10px",
+          width: "100%",
+        }}
+      >
+        <div style={{ width: "70%" }}>
+          <WestSkull1Myth />
+        </div>
+      </div>
       <Navigation prev="/mythjade/northtokenfight" next="/mythjade/stupefy" />
       <Title text="Raining West?" />
       <SouthPuzzle />
