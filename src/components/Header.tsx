89b2a62b59d252f8d51e75f6de6e4d19d4cd0030
Derek Avila <derekavila1086@gmail.com>
2024-06-04 12:37:00 -0500

    header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,70 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
-import Title from "./Text/Title";
+
+function getColor(role: string) {
+  switch (role) {
+    case "Fire 1":
+      return "#db5c5c";
+    case "Fire 2":
+      return "#db5c5c";
+    case "Myth 1":
+      return "#cccc00";
+    case "Myth 2":
+      return "#cccc00";
+    case "Ice":
+      return "#95b7f5";
+    case "Storm 1":
+      return "#b641e0";
+    case "Storm 2":
+      return "#b641e0";
+    case "Storm 3":
+      return "#b641e0";
+    case "Storm 4":
+      return "#b641e0";
+    case "Fire Jade":
+      return "#c3c3bb";
+    case "Myth Jade":
+      return "#c3c3bb";
+    case "Support/Puller":
+      return "#90EE90";
+    case "Storm Jade":
+      return "orange";
+    default:
+      return "inherit";
+  }
+}
+
+function getIcon(role: string) {
+  switch (role) {
+    case "Fire 1":
+      return "/Fire-School.gif";
+    case "Fire 2":
+      return "/Fire-School.gif";
+    case "Myth 1":
+      return "/Myth-School.gif";
+    case "Myth 2":
+      return "/Myth-School.gif";
+    case "Storm 1":
+      return "/Storm-School.gif";
+    case "Storm 2":
+      return "/Storm-School.gif";
+    case "Storm 3":
+      return "/Storm-School.gif";
+    case "Storm 4":
+      return "/Storm-School.gif";
+    case "Fire Jade":
+      return "/Death-School.gif";
+    case "Myth Jade":
+      return "/Death-School.gif";
+    case "Support/Puller":
+      return "/Life-School.gif";
+    case "Storm Jade":
+      return "/Balance-School.gif";
+    default:
+      return "";
+  }
+}
 
 type ClassProps = {
   role: string;
@@ -23,7 +86,40 @@ const Header: React.FC<ClassProps> = ({ role }) => {
           </Toolbar>
         </AppBar>
       </Box>
-      <Title text={role} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "20px 20% 0px 20%",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <img height="30px" src={getIcon(role)} alt="" />
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: getColor(role),
+            }}
+          >
+            {role}
+          </Typography>
+          <img height="30px" src={getIcon(role)} alt="" />
+        </div>
+      </div>
     </>
   );
 };
