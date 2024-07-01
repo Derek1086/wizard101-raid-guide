56530de8ca2187ebb5deeceb3c63f29f7ac6af8c
Derek Avila <derekavila1086@gmail.com>
2024-07-01 10:04:08 -0500

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
             <br />
             Viridian CS Variant - Euxjoy
             <br />
-            Website -Kalani
+            Website - Kalani
           </Typography>
         </Toolbar>
       </AppBar>
